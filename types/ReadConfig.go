e36759539dd3d3b6acd4b16144f9e515764f66c5
FlowBrids <eabour@163.com>
2021-12-29 15:02:55 +0800

    增加初始化日志目录和日志输出到文件

--- types/ReadConfig.go
+++ types/ReadConfig.go
@@ -1,11 +1,13 @@
 package types
 
 import (
+	"io"
 	"log"
 	"os"
 )
 
 type ReadConfig struct {
+	LogDir string
 }
 
 func (ReadConfig) Println() {
@@ -14,4 +16,21 @@ func (ReadConfig) Println() {
 
 func (ReadConfig) Getenv(key string) string {
 	return os.Getenv(key)
+}
+
+func (c *ReadConfig) Init(dir string) {
+	logDir := dir + "logs"
+	_, err := os.Stat(logDir)
+	if err != nil {
+		os.Mkdir(logDir, os.ModePerm)
+		log.Printf("Create logs file: " + logDir)
+	}
+	c.LogDir = logDir
+	logfile, err := os.OpenFile(logDir+"/file-cleaner.log", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0766)
+	if err != nil {
+		log.Fatalf("create log file error" + err.Error())
+	}
+	multiWriter := io.MultiWriter(os.Stdout, logfile)
+	log.SetOutput(multiWriter)
+	log.SetFlags(log.LstdFlags | log.Lshortfile | log.LUTC)
 }
